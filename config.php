daec756df0c96b4a48823be313c9deeabfddf477
dominiksklyarov <dominik@sklyarov.us>
2019-06-10 11:11:47 +0300

    updating config.php again
    for test purpose

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-    $edulog_root = "http://" . $_SERVER['SERVER_NAME'] . "/";
+    //$edulog_root = "http://" . $_SERVER['SERVER_NAME'] . "/";
     /* $edulog_root = "localhost:8080/edulog"; */
 ?>
