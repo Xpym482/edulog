94502f97a90c87e0e57ccb52d880658bfd918176
Jevgeni Prostsenko <43129604+jevgpro@users.noreply.github.com>
2019-06-11 11:14:00 +0300

    test
    commit

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $edulog_root = "http://" . $_SERVER['SERVER_NAME'] . "/";
-    //$edulog_root = "http://localhost:8080/edulog" . "/";
+    $edulog_root = "http://" . $_SERVER['SERVER_NAME'] . "/edulog/";
+    //$edulog_root = "http://localhost:8080/edulog" . "/;
     
 ?>
