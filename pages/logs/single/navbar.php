7f5ea29405baaa08c9e265d8e99b273ee40147c1
Jevgeni Prostsenko <43129604+jevgpro@users.noreply.github.com>
2019-06-20 12:04:26 +0300

    settings added eng version

--- pages/logs/single/navbar.php
+++ pages/logs/single/navbar.php
@@ -19,6 +19,7 @@
         <li><a href="<?php echo $edulog_root . 'pages/tracker';?>">Tundide logimine</a></li>
         <li><a href="<?php echo $edulog_root . 'pages/logs';?>">Tundide logid</a></li>
         <li><a href="<?php echo $edulog_root . 'pages/settings';?>">Seadistused</a></li>
+        <li><a href="<?php echo $edulog_root . 'pages/lesson_room';?>">Lisa ruum</a></li>
         <li><a id="logout" href="<?php echo $edulog_root . 'pages/login/logout.php';?>">Logi välja (<?php echo $_SESSION['user-name']; ?>)</a></li>
 
        <?php if(strstr($_SERVER["SCRIPT_NAME"], 'logs')) : ?>
@@ -35,7 +36,9 @@
     <ul>
         <li><a href="<?php echo $edulog_root . 'pages/tracker';?>">Tracker</a></li>
         <li><a href="<?php echo $edulog_root . 'pages/logs';?>">Logs</a></li>
-        <li><a id="logout" href="<?php echo $edulog_root . 'pages/login';?>">Log out</a></li>
+        <li><a href="<?php echo $edulog_root . 'pages/settings';?>">Settings</a></li>
+        <li><a href="<?php echo $edulog_root . 'pages/lesson_room';?>">Add room</a></li>
+        <li><a id="logout" href="<?php echo $edulog_root . 'pages/login';?>">Log out (<?php echo $_SESSION['user-name']; ?>)</a></li>
         <?php if(strstr($_SERVER["SCRIPT_NAME"], 'logs')) : ?>
         <li><a onclick="fetchCsv('Self')">Download all of your logs</a></li>
         <?php if ($_SESSION['id'] == '3'):?>
